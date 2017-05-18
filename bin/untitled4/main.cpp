7d50d2570481de62ec7abed7712fcd40fdc55ee4
zestroly <zestroly@126.com>
2017-05-18 18:04:51 +0800

    add wiringPi lib

--- bin/untitled4/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <QCoreApplication>
-
-    QCoreApplication a(argc, argv);
-    QSettings *ConfigInt = new QSettings("./camera.ini", QSettings::IniFormat);
-    ConfigInt->setValue("size", 100);
-    ConfigInt->setValue("pos",20);
-    delete ConfigInt;
-
